d5b7b234f14b7ef676fdb950c5198ac482ffa161
AshishKumar05 <ashishgupta.ipec05@gmail.com>
2025-02-08 21:22:39 +0530

    Top News: added unit test

--- app/src/test/java/com/thetopheadlines/topnews/ExampleUnitTest.kt
+++ app/src/test/java/com/thetopheadlines/topnews/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.thetopheadlines.topnews
 
+import com.thetopheadlines.topnews.data.utils.Utils
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -11,7 +12,38 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `formatDate returns correctly formatted date`() {
+        val inputDate = "2025-02-07T03:43:43Z"
+        val expectedOutput = "Feb. 7, 2025"
+        assertEquals(expectedOutput, Utils.formatDate(inputDate))
     }
+
+    @Test
+    fun `formatDate handles invalid date string`() {
+        val inputDate = "invalid-date"
+        val expectedOutput = "Unknown Date"
+        assertEquals(expectedOutput, Utils.formatDate(inputDate))
+    }
+
+    @Test
+    fun `formatDate handles empty date string`() {
+        val inputDate = ""
+        val expectedOutput = "Unknown Date"
+        assertEquals(expectedOutput, Utils.formatDate(inputDate))
+    }
+
+    @Test
+    fun `formatDate handles null date string`() {
+        val inputDate: String? = null
+        val expectedOutput = "Unknown Date"
+        assertEquals(expectedOutput, Utils.formatDate(inputDate ?: ""))
+    }
+
+    @Test
+    fun `formatDate handles invalidFormat date string`() {
+        val inputDate = "03:43:43Z"
+        val expectedOutput = "Unknown Date"
+        assertEquals(expectedOutput, Utils.formatDate(inputDate))
+    }
+
 }
